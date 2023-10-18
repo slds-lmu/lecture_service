e946c8830d4863c3fed42359a19ec468d8fff7f4
Lukas Burk <lukas@quantenbrot.de>
2023-10-18 14:50:19 +0200

    Generalize scripts/run_workflows.sh to more workflows

--- scripts/run_workflows.sh
+++ scripts/run_workflows.sh
@@ -15,15 +15,26 @@ else
   echo "Selected lecture: $lectures"
 fi
 
+run_workflow () {
+  gh workflow run "${1}" --repo "slds-lmu/${2}"
+}
+
 echo ""
 
 for lecture in ${lectures[@]}
 do
-  read -p "Are you sure you want to start workflow render-lecture-slide-status for ${lecture}? (y/n) " -n 1 -r
-  echo    # (optional) move to a new line
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
-      gh workflow run render-lecture-slide-status --repo slds-lmu/${lecture}
-      #echo "gh workflow run render-lecture-slide-status --repo slds-lmu/${lecture}"
-  fi
+  for workflow in update-latex-math.yaml render-lecture-slide-status.yaml fix-figure-paths.yaml
+  do
+    read -p "Do you sure you want to start workflow ${workflow} for ${lecture}? (y/n) " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        #gh workflow run render-lecture-slide-status --repo slds-lmu/${lecture}
+        #gh workflow run update-latex-math --repo slds-lmu/${lecture}
+        run_workflow "${workflow}" "${lecture}"
+        #echo "gh workflow run ${workflow} --repo slds-lmu/${lecture}"
+    fi
+  done
 done
+
+
