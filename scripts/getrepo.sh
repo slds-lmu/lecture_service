a355d7d272ecc15677f035aa7c5d654a2bb9f90e
Lukas Burk <lukas@quantenbrot.de>
2024-03-23 02:19:18 +0100

    Add .ignore

--- scripts/getrepo.sh
+++ scripts/getrepo.sh
@@ -4,18 +4,19 @@
 # No git checkout, just curl
 
 # Set default values for $REPO and $OUT if not already set
-REPO=$1
-OUT=${2:-$REPO}
+REPO="$1"
+OUT="${2:-$REPO}"
 
 # Make sure $REPO is not empty
-if [ -z "$REPO" ]; then
+if [ -z $REPO ]; then
   echo "Usage: $0 REPO [OUTPUT PATH]"
   echo "Example: $0 latex-math"
   echo "Example: $0 latex-math temp-latex-math"
   exit 1
 fi
 
+BRANCH=$(curl -sL https://api.github.com/repos/slds-lmu/$REPO | jq -r '.default_branch')
 # Retrieve the contents of the repository and save them in $OUT
-curl -sL https://api.github.com/repos/slds-lmu/$REPO/tarball/master | tar -xz --directory=$OUT --strip-components=1
+curl -sL https://api.github.com/repos/slds-lmu/$REPO/tarball/"$BRANCH" | tar -xz --directory=$OUT --strip-components=1
 
 echo "Repository $REPO contents saved in $OUT"
