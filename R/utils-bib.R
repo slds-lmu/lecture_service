9a63f3a7ca88a17fbafba6818cc55ffd709b073c
Lukas Burk <lukas@quantenbrot.de>
2025-06-11 13:12:34 +0200

    Docs

--- R/utils-bib.R
+++ R/utils-bib.R
@@ -1,3 +1,42 @@
+#' Convert bib file to formatted list for Markdown output
+#'
+#' @param bib `[character(1)]` Path to bibtex `.bib` file.
+#' @param arrange_by `[character()]` ALl lowercase biblatex field names to sort by. Passed to `dplyr::arrange()`.
+#' @return `character` vector with one element per entry in `bib`.
+#'
+#' @export
+#' @importFrom bib2df bib2df
+#' @importFrom dplyr arrange rename_with mutate coalesce pull
+#'
+bib_to_list <- function(bib, arrange_by = "category") {
+  checkmate::assert_file_exists(bib)
+
+  bib2df::bib2df(bib) |>
+    rename_with(tolower) |>
+    arrange(arrange_by) |>
+    mutate(
+      title = clean_tex_markup(title),
+      note = clean_tex_markup(note),
+      # year = coalesce(.data[["year"]], .data[["date"]]),
+      formatted = glue::glue(
+        "- {format_authors(author)} ({year}): [*{title}*]({url}).\n"
+      ),
+      # Append note if field is provided
+      formatted = ifelse(
+        !is.na(note),
+        glue::glue("{formatted} **Note**: {note}\n"),
+        formatted
+      )
+    ) |>
+    pull(formatted)
+}
+
+#' Condense list of authors from bilatex entry
+#'
+#' - NAs are replaced by ---
+#' - Two authors are separated by &
+#' - More than two authors are replaced by First Author et al.
+#' @keywords internal
 format_authors <- function(author_list) {
   vapply(
     author_list,
@@ -24,44 +63,15 @@ format_authors <- function(author_list) {
   )
 }
 
+#' Clean up tex markup from biblatex entries
+#'
+#' - Removes \\ used for escaping
+#' - Substitutes \emph{foobar} with *foobar*
+#' - Replaces {foo} with foo
+#' @keywords internal
 clean_tex_markup <- function(x) {
-  # x <- bib2df::bib2df(bib_files[[3]])$NOTE
-
   x |>
     stringr::str_remove_all("\\\\") |>
     stringr::str_replace_all("emph\\{(.*)\\}", "*\\1*") |>
     stringr::str_replace_all("\\{.*\\}", "\\1")
-}
-
-#' Convert bib file to formatted list for Markdown output
-#'
-#' @param bib `[character(1)]` Path to bibtex `.bib` file.
-#' @param arrange_by `[character()]` ALl lowercase biblatex field names to sort by. Passed to `dplyr::arrange()`.
-#' @return `character` vector with one element per entry in `bib`.
-#'
-#' @export
-#' @importFrom bib2df bib2df
-#' @importFrom dplyr arrange rename_with mutate coalesce pull
-#'
-bib_to_list <- function(bib, arrange_by = "category") {
-  checkmate::assert_file_exists(bib)
-
-  bib2df::bib2df(bib) |>
-    rename_with(tolower) |>
-    arrange(arrange_by) |>
-    mutate(
-      title = clean_text_markup(title),
-      note = clean_tex_markup(note),
-      year = coalesce(year, date),
-      formatted = glue::glue(
-        "- {format_authors(author)} ({year}): [*{title}*]({url}).\n"
-      ),
-      # Append note if field is provided
-      formatted = ifelse(
-        !is.na(note),
-        glue::glue("{formatted} **Note**: {note}\n"),
-        formatted
-      )
-    ) |>
-    pull(formatted)
 }
